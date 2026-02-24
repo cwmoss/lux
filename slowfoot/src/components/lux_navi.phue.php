5d970fdf07562bf9bdb40296e7f2016775a6a8ed
Robert Wagner <rw@20sec.net>
2026-02-24 16:36:47 +0100

    navi

--- slowfoot/src/components/lux_navi.phue.php
+++ slowfoot/src/components/lux_navi.phue.php
@@ -1,16 +1,3 @@
 <nav>
     <sanity.link :foreach="nav.items as nav" :navitem="nav">link</sanity.link>
 </nav>
-<style>
-    a,
-    a:visited {
-        background-color: yellow;
-        text-decoration: none;
-        font-weight: bold;
-        border-bottom: 6px solid white;
-
-        display: inline-block;
-        line-height: 8px;
-        margin: 0 2em 2em 0;
-    }
-</style>
